3f9f9bae9581f4c726db84ba71eb1e28253cf943
Jonny Paiva <jonny_paiva@hotmail.com.br>
2025-05-05 12:53:24 -0300

    commit

---
+++ sudoku/src/ui/button/CheckGameStatusButton.java
@@ -0,0 +1,13 @@
+package ui.button;
+
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+
+public class CheckGameStatusButton extends JButton {
+
+    public CheckGameStatusButton(final ActionListener actionListener){
+        this.setText("Verificar jogo");
+        this.addActionListener(actionListener);
+    }
+
+}
---
+++ sudoku/src/ui/button/FinishGameButton.java
@@ -0,0 +1,13 @@
+package ui.button;
+
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+
+public class FinishGameButton extends JButton {
+
+    public FinishGameButton(final ActionListener actionListener){
+        this.setText("Concluir");
+        this.addActionListener(actionListener);
+    }
+
+}
